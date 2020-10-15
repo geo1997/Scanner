51bc69833bba9f5731a481b440b22d82c4ca6fd1
kgeor <kgeorgesilva97@gmail.com>
2020-10-15 18:30:38 +0530

    Initial Commit-Android App

---
+++ app/src/main/java/com/technic/qrcode/MainActivity.java
@@ -0,0 +1,38 @@
+package com.technic.qrcode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnTakePicture, btnScanBarcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnScanBarcode.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btnScanBarcode:
+                startActivity(new Intent(MainActivity.this, ScannedBarcodeActivity.class));
+                break;
+        }
+
+    }
+}
